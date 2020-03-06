d23b34e7c71c60c7528720e12e4ddce03c641e36
Andy Block <adblock@mtu.edu>
2020-03-05 21:04:26 -0600

    Plotting Changes

--- FEASolver.py
+++ FEASolver.py
@@ -5,8 +5,9 @@ import sympy as sym
 
 low_boundary = 0 # The lower boundary of the graph
 high_boundary = 1 # The upper boundary of the graph
-grid_points = 20 # The number of nodes that exist along the axes of the boundary (inclusive)
-equ_array_length = 2
+grid_points = 10 # The number of nodes that exist along the axes of the boundary (inclusive)
+equ_array_length = 1
+sub_grid_size = 5
 
 number_of_divisions = (high_boundary - low_boundary) * grid_points #each quadrant will have the same number of nodes in it 
 
@@ -14,15 +15,15 @@ x_range = num.linspace(low_boundary,high_boundary,number_of_divisions)
 y_range = num.linspace(low_boundary,high_boundary,number_of_divisions)
 
 
-elements_with_coordinates =  [[[x_range[xcounter],x_range[xcounter + 1],y_range[ycounter],y_range[ycounter + 1]]
-                for xcounter in range(number_of_divisions - 1) ] for ycounter in range(number_of_divisions - 1)]
+elements_with_coordinates =  [ [[x_range[xcounter],x_range[xcounter + 1],y_range[ycounter],y_range[ycounter + 1]] for xcounter in range(number_of_divisions - 1)] for ycounter in range(number_of_divisions - 1)]
+# print(elements_with_coordinates)
 solution_matrix = [[0 for xcounter in range(number_of_divisions - 1) ] for ycounter in range(number_of_divisions - 1)]
 # print(num.asarray(elements_with_coordinates))
-element_phi_symbols = [[sym.symbols('phi_' + str(xcounter) + str(ycounter)) for xcounter in range(number_of_divisions - 1) ] for ycounter in range(number_of_divisions - 1)]
+# element_phi_symbols = [[sym.symbols('phi_' + str(xcounter) + str(ycounter)) for xcounter in range(number_of_divisions - 1) ] for ycounter in range(number_of_divisions - 1)]
 
 
 
-x,y, f, u, a = sym.symbols('x y f u a')
+x,y, f, u, a, g = sym.symbols('x y f u a g')
 original_function = (1 - x) * (1 - y)
 f = -1
 
@@ -52,34 +53,46 @@ for xcounter in range(number_of_divisions - 1):
         for d in range(len(trial_functions)):
             u += solution_array[d][0] * trial_functions[d] 
         solution_matrix[xcounter][ycounter] += u
-# q = array1 + array2
 
-# print(solution_array)
-# print(rounded_array)
-# print(u)
 
-f = sym.lambdify([x,y],u,"numpy")
+X_Grid = []
+Y_Grid = []
+values = []
+x_plot = []
+vals_plot = []
 
-X_Grid = [[num.average(elements_with_coordinates[xcounter][ycounter][0:2]) 
-          for xcounter in range(number_of_divisions - 1) ] for ycounter in range(number_of_divisions - 1)]
-
-Y_Grid = [[num.average(elements_with_coordinates[xcounter][ycounter][2:4]) 
-          for xcounter in range(number_of_divisions - 1) ] for ycounter in range(number_of_divisions - 1)]
-
-values = [[0 for xcounter in range(number_of_divisions - 1) ] for ycounter in range(number_of_divisions - 1)]
-
-for counter_x in range(len(X_Grid)):
-    for counter_y in range(len(Y_Grid[0])):
-        values[counter_x][counter_y] = f(X_Grid[counter_x][counter_y],Y_Grid[counter_x][counter_y])
-        
-
-# plott.plot(num.diff(values[0]))
-
-# plott.plot(num.diff(values[0]))
+for xcounter in range(len(elements_with_coordinates)): 
+    for ycounter in range(len(elements_with_coordinates[0])):
+#         x_lin = num.linspace(elements_with_coordinates[xcounter][ycounter][0], elements_with_coordinates[xcounter][ycounter][1], sub_grid_size, endpoint=(xcounter == len(elements_with_coordinates) - 1))
+#         y_lin = num.linspace(elements_with_coordinates[xcounter][ycounter][2], elements_with_coordinates[xcounter][ycounter][3], sub_grid_size, endpoint=(ycounter == len(elements_with_coordinates[0]) - 1))
+        x_lin = num.linspace(elements_with_coordinates[xcounter][ycounter][0], elements_with_coordinates[xcounter][ycounter][1], sub_grid_size, endpoint=True)
+        y_lin = num.linspace(elements_with_coordinates[xcounter][ycounter][2], elements_with_coordinates[xcounter][ycounter][3], sub_grid_size, endpoint=True)
+        g = sym.lambdify([x,y],solution_matrix[xcounter][ycounter],"numpy")
+        for list_counter_x in range(len(x_lin)):
+            X_list = []
+            Y_list = []
+            
+            
+            for list_counter_y in range(len(y_lin)):
+                if (y_lin[list_counter_y] == 0):
+                    x_plot.append(x_lin[list_counter_x])
+                    vals_plot.append(g(x_lin[list_counter_x], 0))
+                
+                X_list.append(x_lin[list_counter_x])
+                Y_list.append(y_lin[list_counter_y])
+            X_Grid.append(X_list)
+            Y_Grid.append(Y_list)
+            print(X_list)
+            print(Y_list)
+            
+            
+            vals = [g(X_list[counter], Y_list[counter]) for counter in range(len(X_list))]
+            print(vals)
+            values.append(vals)    
+            print('-----------------------------------------')   
+plott.subplot(121)
+plott.plot(x_plot,vals_plot, '-o')
+plott.subplot(122)
 plott.contourf(X_Grid,Y_Grid,values, 120)
 plott.colorbar()
-
-# print(values)
-# print(X_Grid)
-# print(Y_Grid)
 plott.show()
--- SympyMatrices.py
+++ SympyMatrices.py
@@ -7,6 +7,6 @@ import sympy as sym
 import numpy as num
 
 
-a = [1,2,3,4,5,6,7,8]
+a = num.linspace(0,1,5,endpoint=False)
 
-print (a[0:2])
+print(a)
