eb47c07764a660983a691f43e33d9438500e40df
Andy Block <adblock@mtu.edu>
2020-03-07 11:01:35 -0600

    Changes to Ritz Algorithm

--- RitzSolver.py
+++ RitzSolver.py
@@ -15,8 +15,8 @@ y_range = num.linspace(low_boundary,high_boundary,number_of_divisions)
 X_Grid,Y_Grid = num.meshgrid(x_range,y_range)
 values = num.copy(X_Grid)
 
-equ_array_length = 5
-x,y, f, u, a, phi = sym.symbols('x y f u a phi')
+equ_array_length = 4
+x,y, f, u,a, phi = sym.symbols('x y f u a phi')
 original_function = (1 - x) * (1 - y)
 f = -1
 
@@ -25,14 +25,18 @@ f = -1
 array1 = [x**(n) for n in range(equ_array_length)]
 array2 = [y**(n) for n in range(equ_array_length)]
 
+trial_coefficients = [sym.symbols('a_' + str(counter)) for counter in range(equ_array_length**2)]
+print(sym.latex(sym.Matrix(trial_coefficients)))
+
 trial_functions = [j * k * original_function for k in array1 for j in array2] 
 
 sum_array1 = []
-for trial_counter in range(len(trial_functions)):
+for functional_counter in range(len(trial_functions)):
     phi = 0
-    for trial_counter2 in range(len(trial_functions)):
-        phi = phi + sym.diff(trial_functions[trial_counter],x) * sym.diff(trial_functions[trial_counter2],x) +  sym.diff(trial_functions[trial_counter],y) * sym.diff(trial_functions[trial_counter2],y)
-    sum_array1.append(a * phi + f * trial_functions[trial_counter])
+    for summation_counter in range(len(trial_functions)):
+        phi = phi + sym.diff(trial_functions[functional_counter],x) * sym.diff(trial_functions[summation_counter],x) +  sym.diff(trial_functions[functional_counter],y) * sym.diff(trial_functions[summation_counter],y)
+        sum_array1.append(trial_coefficients[functional_counter] * phi + f * trial_functions[functional_counter])
+        trial_function_counter += 1
     
 solution_array = [sym.solve(2 * sym.integrate(sym.integrate(q,(x,low_boundary,high_boundary)),(y,low_boundary,high_boundary)),a) for q in sum_array1]
 # print(sym.latex(sym.Matrix(solution_array)))
@@ -45,11 +49,6 @@ u = 0
 for d in range(len(trial_functions)):
     u = u + solution_array[d][0] * trial_functions[d] 
     pass
-# q = array1 + array2
-
-# print(solution_array)
-# print(rounded_array)
-# print(u)
 
 f = sym.lambdify([x,y],u,"numpy")
 
@@ -57,13 +56,12 @@ for counter_x in range(len(x_range)):
     for counter_y in range(len(y_range)):
         values[counter_x][counter_y] = f(X_Grid[counter_x][counter_y],Y_Grid[counter_x][counter_y])
         
-print(values[0][0])
 
 # plott.plot(num.diff(values[0]))
-# plott.contourf(X_Grid,Y_Grid,values, 20)
-#plott.colorbar()
+plott.contourf(X_Grid,Y_Grid,values, 120)
+plott.colorbar()
 
 # print(values)
 # print(X_Grid)
 # print(Y_Grid)
-# plott.show()
+plott.show()
