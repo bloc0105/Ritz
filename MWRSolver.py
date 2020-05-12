bc637184517392aab3ea325cf5c5a2605a01156b
Andy Block <adblock@mtu.edu>
2020-05-11 22:08:36 -0500

    This doesnt work yet but Im close

--- MWRSolver.py
+++ MWRSolver.py
@@ -21,41 +21,47 @@ equ_array_length = 3
 x,y, f, u,a, phi = sym.symbols('x y f u a phi')
 original_function = (1 - x) * (1 - y) 
 phi_solved = sym.Derivative(phi,x,x) + sym.Derivative(phi,y,y)
+# print(sym.latex(phi_solved))
 # print(sym.latex(phi_solved.doit()))
 f = -1
 
 
 
-array1 = [x**(n) for n in range(equ_array_length)]
-array2 = [y**(n) for n in range(equ_array_length)]
+array1 = [x**(n + 1) for n in range(equ_array_length)]
+array2 = [y**(n + 1) for n in range(equ_array_length)]
 
 trial_coefficients = [sym.symbols('a_' + str(counter)) for counter in range(equ_array_length**2)]
 trial_functions = [j * k * original_function for k in array1 for j in array2]
-trial_phi = [trial_coefficients[counter] * trial_functions[counter] for counter in range(len(trial_coefficients))]
+trial_equations = [trial_coefficients[counter] * trial_functions[counter] for counter in range(len(trial_coefficients))]
+trial_phi = sum(trial_equations) + original_function
 
 coefficients = []
+eqs = []
 # print(sym.latex(sym.Matrix(trial_phi)))
-for counter  in range(len(trial_phi)):
-    residual = phi_solved.subs(phi,trial_phi[counter]).doit() - f
-    current_var = trial_coefficients[counter]
+residual = phi_solved.subs(phi,trial_phi).doit() - f
+
+# print(sym.latex(residual))
+solution_equations = []
+for counter  in range(len(trial_equations)):
     weighted_average = sym.integrate(sym.integrate(residual * trial_functions[counter],(x,low_boundary,high_boundary)),(y,low_boundary,high_boundary))
-#     print(sym.latex(weighted_average))
-    ansewr = sym.solve(weighted_average,current_var)
-    coefficients.append(ansewr)
+    solution_equations.append(weighted_average)
     
-    pass
+# print(sym.latex(sym.Matrix(solution_equations)))
 
-
+result_set = sym.linsolve(solution_equations,trial_coefficients)
+print(sym.latex(result_set))
 # print(sym.latex(sym.Matrix(coefficients)))
 # print(sym.latex(sym.Matrix(trial_functions)))
 
-print(len(coefficients))
-print(len(trial_functions))
+# print(coefficients)
+# print(len(trial_functions))
 
-for counter in range(len(coefficients)):
-#     print(coefficients[counter][0] * trial_functions[counter])
-    print(sym.latex(coefficients[counter][0]))
-    print(trial_functions[counter])
+u = original_function
+for d in range(len(result_set.args[0])):
+    u = u + result_set.args[0][d] * trial_functions[d] 
+
+
+
 f = sym.lambdify([x,y],u,"numpy")
  
 for counter_x in range(len(x_range)):
